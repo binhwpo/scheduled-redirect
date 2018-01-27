af0ee6dda2868d71de0d84483a68ed8a85b6588c
Binh WPOS <wpo@binh.vn>
2018-01-27 11:43:31 +0700

    Init

---
+++ scheduled-redirect.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Scheduled Redirect
+ * Plugin URI:  https://wpos.org/#scheduled-redirect
+ * Description: This plugin provides several redirection action for Scheduled Content Actions plugin.
+ * Version:     1.2
+ * Author:      Binh Nguyen
+ * Author URI:  http://www.binh.vn/
+ * Text Domain: scheduled-redirect
+ */
+
