ef6b511529eb104f0c52566331469b93bd4c7eb9
Binh WPOS <wpo@binh.vn>
2018-02-06 21:04:56 +0700

    Plugin Description: Change some text

--- scheduled-redirect.php
+++ scheduled-redirect.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Scheduled Redirect
  * Plugin URI:  https://wpos.org/#scheduled-redirect
- * Description: This plugin provides several redirection action for Scheduled Content Actions plugin.
+ * Description: This plugin provides additional redirect action for Scheduled Content Actions plugin.
  * Version:     1.2
  * Author:      Binh Nguyen
  * Author URI:  http://www.binh.vn/
